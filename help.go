f4341690a9c0bd5bc5360b111041927806fcba6a
N3N <narenarya@live.com>
2025-08-17 17:17:13 -0700

    feat: add opnesource license

--- help.go
+++ help.go
@@ -1,14 +1,35 @@
+// Copyright 2025 Naren Yellavula
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
-import markdown "github.com/MichaelMure/go-term-markdown"
+import (
+	"fmt"
+	"runtime"
+
+	markdown "github.com/MichaelMure/go-term-markdown"
+)
 
 func getHelpMessage() string {
-	message := `
+	message := fmt.Sprintf(`
 
- **Recaller**
+ **Recaller %s**
 
 Easily access & re-run your most frequent shell history with blazing-fast search and documentation help.
 No more cycling back with <bck-isearch>. See the latest history for any shell command.
+
+Built with Go %s
 
 # 1. Features
 * Recall your shell commands based on recency & frequency
@@ -23,10 +44,14 @@ No more cycling back with <bck-isearch>. See the latest history for any shell co
 * Bash
 * Zshell (Zsh)
 
-# Pleas be aware
-* Copy to cliboard feature on Linux or Unix requires 'xclip' or 'xsel' command to be installed
+# Please be aware
+* Copy to clipboard feature on Linux or Unix requires 'xclip' or 'xsel' command to be installed
 
-`
+# License
+Licensed under the Apache License, Version 2.0
+Copyright © 2025 Naren Yellavula
+
+`, version, runtime.Version())
 	result := markdown.Render(string(message), 80, 3)
 	return string(result)
 }
